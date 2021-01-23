2bd31a83611a587dc50a6c3bb85478c435830945
apsamuel <aaron.psamuel@gmail.com>
2021-01-23 15:10:17 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Grid } from "@material-ui/core";
+import Header from "./Header";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const pallette = {
+  paper: {
+    gray: "#363945"
+  }
+};
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: pallette.paper.gray,
+    padding: 10
+  },
+  item: {
+    backgroundColor: pallette.paper.gray
+  }
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      spacing={5}
+      className={classes.container}
+      direction="column"
+    >
+      <Grid item space={10}>
+        <Header />
+      </Grid>
+      <Grid item container spacing={5} className={classes.item}>
+        <Grid item spacing={5} xs={0} sm={2} />
+        <Grid item spacing={5} xs={12} sm={8}>
+          Content
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
